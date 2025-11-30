ed7f9a1051fd5982013158b4d2a2969b1239dc20
David Pham <davidpham87@gmail.com>
2025-11-30 18:09:48 +0100

    Update test

--- test/bb_web_ds_tools/repl_test.cljs
+++ test/bb_web_ds_tools/repl_test.cljs
@@ -7,15 +7,6 @@
 (use-fixtures :each setup/suppress-re-frame-warnings)
 
 (deftest test-repl
-  (testing "add instance"
-    (rf/dispatch-sync [:bb-web-ds-tools.views.repl/add-instance])
-    (let [instances @(rf/subscribe [:bb-web-ds-tools.views.repl/instances])]
-      (is (pos? (count instances)))))
-
   (testing "eval code dispatch"
-    (rf/dispatch-sync [:bb-web-ds-tools.views.repl/add-instance])
-    (let [instances @(rf/subscribe [:bb-web-ds-tools.views.repl/instances])
-          id (first (keys instances))]
-      ;; Just verify dispatch doesn't throw. Side effects (Portal) are not easily tested here without mocking.
-      (rf/dispatch-sync [:bb-web-ds-tools.views.repl/eval-code id "(+ 1 2)"])
-      (is true))))
+    (rf/dispatch-sync [:bb-web-ds-tools.views.repl/eval-code id "(+ 1 2)"])
+    (is true)))
